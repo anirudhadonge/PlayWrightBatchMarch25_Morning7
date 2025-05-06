bfb63d0304d1fc9efd31dbf9d8e7f738ebdfd8bb
anirudha <10610321+anirudhadonge@users.noreply.github.com>
2025-05-06 07:23:16 +0530

    commented the line of code

--- PlaywrightDemo/GlobalSetup/GlobalSetup.ts
+++ PlaywrightDemo/GlobalSetup/GlobalSetup.ts
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv'
 
 export default function setEnviromentVariable(){
-    console.log(process.env);
+    //console.log(process.env);
     dotenv.config({
         path:'./.env',
         override:true
     })
-    console.log(process.env);
+    //console.log(process.env);
 }
